17cfe6adc95d88b5515b4f57a7eec03b01e5325a
Wachira-Macharia <eugenecomboni@gmail.com>
2023-01-10 09:40:13 +0300

    Project Setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import react from 'react';
+import reactDom from 'react-dom/client';
+
+import App from './App';
+import './index.css';
+
+const root = reactDom.createRoot(document.getElementById('root'));
+
+root.render(<App />); 
