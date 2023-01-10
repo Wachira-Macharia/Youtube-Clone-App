d65c9475165530e704396f75f1e9014fac724f2c
Wachira-Macharia <eugenecomboni@gmail.com>
2023-01-10 12:28:12 +0300

    SearchBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => (
     <Stack 
@@ -13,6 +14,7 @@ const Navbar = () => (
       <Link to="/" style={{ display: 'flex', alignitems: 'center'}}>
         <img src={logo} alt={logo} height={45}/>
       </Link>
+      <SearchBar />
     </Stack>
   )
 
